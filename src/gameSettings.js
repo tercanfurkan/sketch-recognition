5df7c26ae826dc3941c2b082917cf233f47dcf07
Ömer Tercan <omer.tercan@relexsolutions.com>
2020-05-04 22:48:20 +0300

    skecher applier approach 1, wip

--- src/gameSettings.js
+++
@@ -1,6 +0,0 @@
-const labels = require("./labels.json"); // TensorFlow models
-const initialRounds = 10; //how many rounds
-const questions = labels; //questions
-const duration = 20; //time in sec to solve round
-
-export { initialRounds, duration, labels, questions };
